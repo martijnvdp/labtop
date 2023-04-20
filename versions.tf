b8e5b0b556f34430f7020efd32062de7a66682ef
Sasa Modrakovic <shetkantoor@gmail.com>
2023-04-20 13:55:25 +0200

    upgrade helm and king

--- versions.tf
+++ versions.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     helm = {
       source  = "hashicorp/helm"
-      version = ">= 2.2.0"
+      version = ">= 2.9.0"
     }
     kind = {
       source  = "tehcyx/kind"
-      version = "0.0.15"
+      version = "0.0.17"
     }
   }
 }
